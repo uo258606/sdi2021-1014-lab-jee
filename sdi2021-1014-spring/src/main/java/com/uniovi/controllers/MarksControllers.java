dbc016fb0a1354b0c9007b0210ef10d1f41d6590
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-02-14 22:03:43 +0100

    SDI-2021-1014-2.3-Beans, servicios e inyección de dependencias.

--- sdi2021-1014-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,24 +1,35 @@
 package com.uniovi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
+import com.uniovi.services.*;
 
 @RestController
 public class MarksControllers {
 
+	@Autowired // Inyectar el servicio
+	private MarksService marksService;
+
 	@RequestMapping("/mark/list")
 	public String getList() {
-		return "Getting List";
+		return marksService.getMarks().toString();
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
 	public String setMark(@ModelAttribute Mark mark) {
-		return "added: " + mark.getDescription() + " with score : " + mark.getScore() + " id: " + mark.getId();
+		marksService.addMark(mark);
+		return "Ok";
 	}
 
 	@RequestMapping("/mark/details/{id}")
 	public String getDetail(@PathVariable Long id) {
-		return " Getting Detail: " + id;
+		return marksService.getMark(id).toString();
 	}
 
+	@RequestMapping("/mark/delete/{id}")
+	public String deleteMark(@PathVariable Long id) {
+		marksService.deleteMark(id);
+		return "Ok";
+	}
 }
