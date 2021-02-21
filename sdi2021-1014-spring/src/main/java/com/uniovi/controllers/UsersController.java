e3f97e7eabb5af39ecd78b352adcdf1d74e2ad92
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-02-21 18:20:51 +0100

    SDI-2021-1014-3.4-Autenticación y control de acceso

--- sdi2021-1014-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
-import com.uniovi.services.UsersService;
+import com.uniovi.services.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.context.*;
 
 @Controller
 public class UsersController {
+
 	@Autowired
 	private UsersService usersService;
+
+	@Autowired
+	private SecurityService securityService;
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
@@ -54,5 +60,31 @@ public class UsersController {
 		user.setId(id);
 		usersService.addUser(user);
 		return "redirect:/user/details/" + id;
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup() {
+		return "signup";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@ModelAttribute("user") User user, Model model) {
+		usersService.addUser(user);
+		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+		return "redirect:home";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
+	}
+
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		User activeUser = usersService.getUserByDni(dni);
+		model.addAttribute("markList", activeUser.getMarks());
+		return "home";
 	}
 }
--- sdi2021-1014-spring/src/main/java/com/uniovi/services/UsersService.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/services/UsersService.java
@@ -5,12 +5,16 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.uniovi.entities.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.uniovi.repositories.UsersRepository;
 
 @Service
 public class UsersService {
 	@Autowired
 	private UsersRepository usersRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@PostConstruct
 	public void init() {
@@ -27,7 +31,12 @@ public class UsersService {
 	}
 
 	public void addUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		usersRepository.save(user);
+	}
+
+	public User getUserByDni(String dni) {
+		return usersRepository.findByDni(dni);
 	}
 
 	public void deleteUser(Long id) {
