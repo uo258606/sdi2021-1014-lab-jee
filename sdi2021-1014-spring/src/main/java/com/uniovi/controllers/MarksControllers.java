8a19e63928f5f3d24a3eca5e7ce49958eabac833
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-02-28 23:00:49 +0100

    SDI-2021-1014-4.5-Búsqueda.

--- sdi2021-1014-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -28,10 +28,15 @@ public class MarksControllers {
 	private HttpSession httpSession;
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model, Principal principal) {
+	public String getList(Model model, Principal principal,
+			@RequestParam(value = "", required = false) String searchText) {
 		String dni = principal.getName(); // DNI es el name de la autenticación
 		User user = usersService.getUserByDni(dni);
-		model.addAttribute("markList", marksService.getMarksForUser(user));
+		if (searchText != null && !searchText.isEmpty()) {
+			model.addAttribute("markList", marksService.searchMarksByDescriptionAndNameForUser(searchText, user));
+		} else {
+			model.addAttribute("markList", marksService.getMarksForUser(user));
+		}
 		return "mark/list";
 	}
 
@@ -83,7 +88,7 @@ public class MarksControllers {
 	}
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model, Principal principal){
+	public String updateList(Model model, Principal principal) {
 		String dni = principal.getName(); // DNI es el name de la autenticación
 		User user = usersService.getUserByDni(dni);
 		model.addAttribute("markList", marksService.getMarksForUser(user));
