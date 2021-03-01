efa5ae936e958e5151e179a3bf546288b4df339a
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-03-01 12:46:53 +0100

    SDI-2021-1014-sesión4-Ejercicio-complementario4

--- sdi2021-1014-spring/src/main/java/com/uniovi/repositories/UsersRepository.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -4,12 +4,16 @@ import com.uniovi.entities.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.*;
 import java.util.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
 	
 	User findByDni(String dni);
 	
 	@Query("SELECT r FROM User r WHERE (LOWER(r.name) LIKE LOWER(?1) OR LOWER(r.lastName)LIKE LOWER(?1))")
-	List<User> searchByNameAndSurname(String searchText);
+	Page<User> searchByNameAndSurname(Pageable pageable, String searchText);
+	
+	Page<User> findAll(Pageable pageable);
 
 }
--- sdi2021-1014-spring/src/main/java/com/uniovi/services/UsersService.java
+++ sdi2021-1014-spring/src/main/java/com/uniovi/services/UsersService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.uniovi.entities.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.uniovi.repositories.UsersRepository;
+import org.springframework.data.domain.*;
 
 @Service
 public class UsersService {
@@ -23,6 +24,11 @@ public class UsersService {
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<User>();
 		usersRepository.findAll().forEach(users::add);
+		return users;
+	}
+	
+	public Page<User> getUsers(Pageable pageable) {
+		Page<User> users = usersRepository.findAll(pageable);
 		return users;
 	}
 
@@ -43,10 +49,10 @@ public class UsersService {
 		usersRepository.deleteById(id);
 	}
 
-	public List<User> searchUsersByNameAndSurname(String searchText) {
-		List<User> users = new ArrayList<User>();
+	public Page<User> searchUsersByNameAndSurname(Pageable pageable, String searchText) {
+		Page<User> users =  new PageImpl<User>(new LinkedList<User>());
 		searchText = "%" + searchText + "%";
-		users = usersRepository.searchByNameAndSurname(searchText);
+		users = usersRepository.searchByNameAndSurname(pageable, searchText);
 
 		return users;
 	}
