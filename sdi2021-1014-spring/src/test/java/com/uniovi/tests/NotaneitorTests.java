c0870801dbbb17e646be6715332f59395f17aa16
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-03-08 02:04:52 +0100

    SDI-2021-1014-5.6-Casos Uso VistaLogin.

--- sdi2021-1014-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-1014-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -120,5 +120,60 @@ public class NotaneitorTests {
 		// Rellenamos el formulario.
 		PO_RegisterView.fillForm(driver, "99999990B", "Josefo", "Per", "77777", "77777");
 	}
-
+	
+	//PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR07() {
+	//Vamos al formulario de logueo.
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	//Rellenamos el formulario
+	PO_LoginView.fillForm(driver, "99999990A" , "123456" );
+	//COmprobamos que entramos en la pagina privada de Alumno
+	PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+	
+	//PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR08() {
+	//Vamos al formulario de logueo.
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	//Rellenamos el formulario
+	PO_LoginView.fillForm(driver, " 99999993D" , "123456" );
+	//COmprobamos que entramos en la pagina privada de Alumno
+	PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+	
+	//PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR09() {
+	//Vamos al formulario de logueo.
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	//Rellenamos el formulario
+	PO_LoginView.fillForm(driver, "99999988F" , "123456" );
+	//COmprobamos que entramos en la pagina privada de Alumno
+	PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+	
+	//PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR10() {
+	//Vamos al formulario de logueo.
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	//Rellenamos el formulario
+	PO_LoginView.fillForm(driver, "99999990A" , "123456" );
+	//COmprobamos que entramos en la pagina privada de Alumno
+	PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+	
+	//PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR11() {
+	//Vamos al formulario de logueo.
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	//Rellenamos el formulario
+	PO_LoginView.fillForm(driver, "99999990A" , "123456" );
+	//COmprobamos que entramos en la pagina privada de Alumno
+	PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+	
 }
